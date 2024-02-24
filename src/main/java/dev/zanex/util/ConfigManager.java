3f39aeb81efab554c4adfae6a784bcd126e5f796
MoritzMCC <88977881+MoritzMCC@users.noreply.github.com>
2024-02-25 06:35:13 +1300

    configManager
    -createConfig
    -addDataString
    -getDataString

--- src/main/java/dev/zanex/util/ConfigManager.java
+++ src/main/java/dev/zanex/util/ConfigManager.java
@@ -3,5 +3,18 @@ package dev.zanex.util;
 
 
 public class ConfigManager {
-    
+
+    public void createConfig(String configName){
+        Config config = new Config(configName);
+    }
+
+    public void addDataString(String configName,String dataGroupName, String dataType, String dataString){
+        Config config = new Config(configName);
+        config.add(dataGroupName, dataType, dataString);
+    }
+
+    public String getDataString(String configName,String dataGroupName, String dataType){
+        Config config = new Config(configName);
+        return config.getData(dataGroupName, dataType);
+    }
 }
