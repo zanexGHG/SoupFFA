c6a651996391d71eca4c042ce05af22d5ebd49b6
MoritzMCC <88977881+MoritzMCC@users.noreply.github.com>
2024-02-25 05:35:02 +1300

    started with BuildCommand
    -started with BuildCommand
    -started with DisableEventListener

---
+++ src/main/java/dev/zanex/commands/BuildCommand.java
@@ -0,0 +1,47 @@
+package dev.zanex.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuildCommand implements CommandExecutor {
+
+  public List<Player> playersInBuildMode = new ArrayList<>();
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(!(sender instanceof Player)){
+            sender.sendMessage("you have to be a Player to execute this command");
+            return false;
+        }
+        Player player =(Player) sender;
+
+        if (!player.hasPermission("SoupFFa.command.build")){
+           player.sendMessage("You don't have the permissions to execute this command");
+           return false;
+        }
+
+        if (args.length ==1){
+            try{
+                Bukkit.getPlayer(args[1]);
+
+            }catch ()
+        }
+
+
+          return true;
+    }
+
+    private void activateBuildMode(Player player){
+
+    }
+
+    public List<Player> getPlayersInBuildMode() {
+        return playersInBuildMode;
+    }
+}
